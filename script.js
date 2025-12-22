3ed40737158f4c21104c71e29060871b2c20c941
Jessica Wong <116975948+jess-icaww@users.noreply.github.com>
2025-12-21 20:36:26 -0500

    Add random recipe fetching and display

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello");
+async function getRandomRecipe() {
+    try {
+        const response = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}`);
+        const data = await response.json();
+        console.log(data.recipes[0]);
+
+        const recipe = data.recipes[0];
+        const container = document.getElementById(`recipe-container`);
+        container.innerHTML = `<h1>${recipe.title}</h1>
+        <img src=${recipe.image}>`;
+    } catch (error) {
+        console.error(`Error. Try again.`);
+    }
+}
+
+getRandomRecipe();
